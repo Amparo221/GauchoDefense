16d8ed74ab5ef6b76183a5eeb64ece06ee51b48f
Amparo <amparo.moreno221@gmail.com>
2025-07-05 21:50:24 -0300

    adicion de animaciones

--- jugador.py
+++ jugador.py
@@ -15,21 +15,27 @@ gaucho_velocidad = 5
 
 def movimiento_jugador(y_actual, gaucho_velocidad):
     tecla_presionada = pygame.key.get_pressed()
+    movimiento = False
+    
     if tecla_presionada[K_w]:
         y_actual -= gaucho_velocidad
+        movimiento = True
     if tecla_presionada[K_s]:
         y_actual += gaucho_velocidad
+        movimiento = True
 
     if y_actual < 0:
         y_actual = 0
     if y_actual > 600 - gaucho_size[1]:
         y_actual = 600 - gaucho_size[1]
         
-    return y_actual
+    return y_actual, movimiento
+
 
 
 def disparar_balas(lista_de_balas, tiempo_actual, ultimo_disparo, cooldown, jugador_x, jugador_y, jugador_size):
-    bala_velocidad = 10
+    disparar=False
+    bala_velocidad = 15
     for bala in lista_de_balas[:]:
         bala[0] += bala_velocidad
         if bala[0] > 1000:
@@ -37,10 +43,14 @@ def disparar_balas(lista_de_balas, tiempo_actual, ultimo_disparo, cooldown, juga
     
     tecla_presionada = pygame.key.get_pressed()
     if tecla_presionada[K_SPACE] and tiempo_actual - ultimo_disparo >= cooldown:
+        disparar=True
         bala_x = jugador_x + jugador_size[0]
-        bala_y = jugador_y + (jugador_size[1] // 2)
+        bala_y = jugador_y + ((jugador_size[1] // 2)-15)
         lista_de_balas.append([bala_x, bala_y])
-        return tiempo_actual
+        return tiempo_actual, disparar
+
+    if tiempo_actual - ultimo_disparo < 300:
+        return ultimo_disparo, disparar
     
-    return ultimo_disparo
+    return ultimo_disparo, disparar
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, gif_pygame
 from pygame.locals import *
 from jugador import *
 
@@ -8,20 +8,42 @@ pantalla = pygame.display.set_mode((1000, 600))
 fondo = pygame.image.load("GauchoDefense/assets/fondo.png").convert()
 ancho_fondo, altura_fondo = fondo.get_size()  
 
-gaucho = pygame.image.load("GauchoDefense/assets/default.png").convert_alpha()
-gaucho_size=(150,150)
+gaucho = pygame.image.load("GauchoDefense/assets/gaucho.png").convert_alpha()
+gaucho_size=(155,175)
 gaucho = pygame.transform.scale(gaucho, gaucho_size)
 gaucho_x = 0
 gaucho_y = 0
 gaucho_velocidad = 5
 
+caminar = gif_pygame.load("GauchoDefense/assets/caminar_2.gif")
+disparo = gif_pygame.load("GauchoDefense/assets/disparo_3.gif")
+
+tecla_presionada = pygame.key.get_pressed()
 
 # BALAS
 bala_img = pygame.Surface((15, 5))  
-bala_img.fill((250, 0, 0)) 
+bala_img.fill((0, 0, 0)) 
 balas = []  #Lista pa las balas
 ultimo_disparo = 0  
 cooldown = 400  
+
+
+
+disparo_playing = False
+disparo_start_time = 0
+disparo_duration = 400  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def crear_fondo():
@@ -37,7 +59,8 @@ def crear_balas():
 
 running = True
 reloj = pygame.time.Clock()
-
+movimiento = False
+disparar = False
 
 while running:
 
@@ -49,10 +72,10 @@ while running:
             running = False
             
         
-    gaucho_y = movimiento_jugador(gaucho_y, gaucho_velocidad)
+    gaucho_y, is_moving = movimiento_jugador(gaucho_y, gaucho_velocidad)
 
     
-    ultimo_disparo = disparar_balas(
+    ultimo_disparo, disparar = disparar_balas(
         balas, 
         tiempo_actual, 
         ultimo_disparo, 
@@ -66,7 +89,22 @@ while running:
     crear_fondo()
     crear_balas()
     
-    pantalla.blit(gaucho, (gaucho_x, gaucho_y))
+    if disparar:
+        disparo_playing = True
+        disparo_start_time = tiempo_actual
+        disparo.reset() 
+    if disparo_playing:
+        
+        disparo.render(pantalla, (gaucho_x, gaucho_y))
+    
+        if tiempo_actual - disparo_start_time >= disparo_duration:
+            disparo_playing = False
+    elif is_moving:
+        caminar.render(pantalla, (gaucho_x, gaucho_y))
+    else:
+        pantalla.blit(gaucho, (gaucho_x, gaucho_y))
+
+
 
      
     pygame.display.flip()
